5faa56ee4140bc2dfb20624c74da632083347211
ICQRX <ICQRX@QUOCNGUYEN>
2014-08-05 21:23:43 -0700

    update

--- customplugin/src/customplugin/projects/CustomProjectSupport.java
+++ customplugin/src/customplugin/projects/CustomProjectSupport.java
@@ -1,15 +1,29 @@
 package customplugin.projects;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
 
 import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.part.FileEditorInput;
 
 import customplugin.natures.ProjectNature;
 
@@ -35,22 +49,29 @@ public class CustomProjectSupport {
         try {
             addNature(project);
             String[] paths = {
-                    "hooks",
-                    "merges",
-                    "platforms",
-                    "plugins",
-                    "www"
+                    "source",
+                    "design",
+                    "bin"
                     };
             addToProjectStructure(project, paths);
-
-        } catch (CoreException e) {
+            // open brower to load maqetta
+            loadBrowser();
+        } catch (Exception e) {
             e.printStackTrace();
             project = null;
         }
  
         return project;
     }
- 
+    /**
+     * @throws MalformedURLException 
+     * @throws PartInitException 
+     * 
+     */
+    public static void loadBrowser() throws PartInitException, MalformedURLException {
+    	   IWorkbenchBrowserSupport browserSupport = PlatformUI.getWorkbench().getBrowserSupport();
+           browserSupport.getExternalBrowser().openURL(new URL("http://163.180.117.219:50000/"));
+    }
     /**
      * Just do the basics: create a basic project.
      *
@@ -104,15 +125,42 @@ public class CustomProjectSupport {
      * @param newProject
      * @param paths
      * @throws CoreException
+     * @throws MalformedURLException 
      */
-    private static void addToProjectStructure(IProject newProject, String[] paths) throws CoreException {
+    private static void addToProjectStructure(IProject newProject, String[] paths) throws CoreException, MalformedURLException {
         for (String path : paths) {
             IFolder etcFolders = newProject.getFolder(path);
             createFolder(etcFolders);
+            //create file in design folder
+            if(path.equalsIgnoreCase("design")) {
+            	createDesignFile(etcFolders);
+            }
         }
     }
     
     /**
+     * Create design file layout
+     * @param etcFolders
+     * @throws MalformedURLException 
+     */
+    private static void createDesignFile(IFolder etcFolders) throws MalformedURLException {
+		IFile designFile = etcFolders.getFile("gui.html");
+		if (!designFile.exists()) {
+		    try {
+				byte[] bytes = "File contents".getBytes();
+				InputStream source = new ByteArrayInputStream(bytes);
+				designFile.create(source, IResource.NONE, null);
+//				IEditorInput editorInput = new FileEditorInput(designFile);
+//				IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+//			    IWorkbenchPage page = window.getActivePage();
+//			    IDE.openEditor(page, designFile, "org.eclipse.ui.browser.editor");
+			} catch (CoreException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
      * 
      * @param project
      * @throws CoreException
